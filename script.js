7cd05fe7a95d2aede0eaa463663d8bb2a22d9432
Jacob1437 <Jacob.leader06@gmail.com>
2024-05-25 13:52:19 -0700

    Feat: info

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // script.js
 const countdown = () => {
+    extraCount();
     const graduationDate = new Date('June 22, 2024 00:00:00').getTime();
     const now = new Date().getTime();
     const timeLeft = graduationDate - now;
@@ -25,12 +26,41 @@ const extraCount = () => {
     const graduationDate = new Date('June 22, 2024 00:00:00').getTime();
     const now = new Date().getTime();
 
-    const satCount = countDays(now, graduationDate, 6); // Saturday is represented by 6
-    const wedCount = countDays(now, graduationDate, 3); 
+    interhouseDates = [
+        new Date('June 4, 2024 00:00:00'), // Soccer
+        new Date('June 8, 2024 00:00:00')]; // Rugby
 
-    
+
+    const dayCounts = [];
+    for (let i = 1; i <= 7; i++) {
+        dayCounts[i-1] = countDays(now, graduationDate, i); // Counts occurances of days between today and grad, mon = 1, sun = 7
+    }
+
+    // TODO update as the day goes on
+    document.getElementById("chapelCount").innerHTML = (dayCounts[2] + dayCounts[5]) + " Chapels Left";
+    document.getElementById("sportCount").innerHTML = (dayCounts[1] + dayCounts[3] + dayCounts[5]) + " Sports Left";
+    document.getElementById("360Count").innerHTML = (dayCounts[0] + dayCounts[2]) + " 360s Left";
+    let weekDayCount = 0; 
+    dayCounts.slice(0,5).forEach( num => { weekDayCount += num;})
+    document.getElementById("blockCount").innerHTML = (weekDayCount * 4) + " Blocks Left";
+
+    let IHCount = 0;
+    interhouseDates.forEach(date => {
+        if (date.getTime() > now) {
+            IHCount++;
+        }
+    });
+    document.getElementById("IHCount").innerHTML = (IHCount) + " Interhouses Left";
+
+    const timeLeft = graduationDate - now;
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    document.getElementById("mealCount").innerHTML = ((days * 3) - dayCounts[6]) + " Meals Left"; // subtract suns cuz there is only 2 meals
+
 }
 
+window.onload = function() {
+    extraCount();
+};
 
 const countDays = (startDate, endDate, dayOfWeek) => {
   let count = 0;
