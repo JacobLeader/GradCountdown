a317ec1e313c460f161f8b299e270b6d753155ca
Jacob1437 <Jacob.leader06@gmail.com>
2024-05-26 14:30:39 -0700

    Refactor, Fix: Time of Day Accurate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 const countdown = () => {
-    extraCount();
+    getInfo();
     const graduationDate = new Date('June 22, 2024 00:00:00').getTime();
     const now = new Date().getTime();
     const timeLeft = graduationDate - now;
@@ -22,46 +22,121 @@ const countdown = () => {
 
 setInterval(countdown, 1000);
 
-
-const extraCount = () => {
+const getInfo = () => {
     const graduationDate = new Date('June 22, 2024 00:00:00').getTime();
-    const now = new Date().getTime();
-
-    interhouseDates = [
-        new Date('June 4, 2024 00:00:00'), // Soccer
-        new Date('June 8, 2024 00:00:00')]; // Rugby
-
+    const currentDate = new Date
+    const now = currentDate.getTime();
+    const isWeekday = currentDate.getDay() >= 1 && currentDate.getDay() <= 5;
 
     const dayCounts = [];
-    for (let i = 1; i <= 7; i++) {
-        dayCounts[i-1] = countDays(now, graduationDate, i); // Counts occurances of days between today and grad, mon = 1, sun = 7
+    for (let i = 0; i < 7; i++) {
+        dayCounts[i] = countDays(now, graduationDate, i); // Counts occurances of days between today and grad, mon = 1, sun = 7
     }
+    let weekDayCount = 0; 
+    dayCounts.slice(0,5).forEach( num => { weekDayCount += num;});
+
+    const blockTimes = [
+        { startTime: '8:45', endTime: '9:55' },
+        { startTime: '10:00', endTime: '11:15' },
+        { startTime: '11:40', endTime: '13:30' },
+        { startTime: '14:00', endTime: '15:10' }
+    ];    
+    const mealTimes = [
+        { startTime: '7:45', endTime: '8:45' },
+        { startTime: '13:10', endTime: '13:30' },
+        { startTime: '18:00', endTime: '18:45' }
+    ];    
+    const sunMealTimes = [
+        { startTime: '9:00', endTime: '12:00' },
+        { startTime: '17:00', endTime: '17:45' }
+    ];    
+    interhouseDates = [
+        new Date('June 4, 2024 12:00:00'), // Soccer
+        new Date('June 8, 2024 12:00:00')  // Rugby
+    ]; 
+
+
+
 
     // TODO update as the day goes on
-    document.getElementById("chapelCount").innerHTML = (dayCounts[2] + dayCounts[5]);
-    document.getElementById("sportCount").innerHTML = (dayCounts[1] + dayCounts[3] + dayCounts[5]);
-    document.getElementById("360Count").innerHTML = (dayCounts[0] + dayCounts[2]);
-    let weekDayCount = 0; 
-    dayCounts.slice(0,5).forEach( num => { weekDayCount += num;})
-    document.getElementById("blockCount").innerHTML = weekDayCount * 4;
+    document.getElementById("chapelCount").innerHTML = getChapelsLeft(dayCounts, currentDate.getDay());
+    document.getElementById("sportCount").innerHTML = getSportsLeft(dayCounts, currentDate.getDay());
+    document.getElementById("360Count").innerHTML = get360Left(dayCounts, currentDate.getDay());
+    document.getElementById("blockCount").innerHTML = getBlocksLeft(weekDayCount, isWeekday, blockTimes);
+    document.getElementById("IHCount").innerHTML = getIHLeft(interhouseDates, now);
+    document.getElementById("mealCount").innerHTML = getMealsLeft(weekDayCount, dayCounts, mealTimes, sunMealTimes, currentDate.getDay()); 
+}
 
+//! Day starts on Sunday: Sun = 0, Mon = 1, Tue = 2, Wed = 3, Thu = 4, Fri = 5, Sat = 6
+function getChapelsLeft(dayCounts, currentDay) {
+    const wedTime = { startTime: '13:30', endTime: '14:00' };
+    const satTime = { startTime: '11:15', endTime: '12:00' };
+    console.log(dayCounts);
+    if (currentDay == 2) {
+        const chapelLeftInDay = getRemainingInstances(wedTime);
+        return chapelLeftInDay + dayCounts[3] + dayCounts[6] - 1;
+    } else if (currentDay == 5) {
+        const chapelLeftInDay = getRemainingInstances(satTime);
+        return chapelLeftInDay + dayCounts[3] + dayCounts[6] - 1;
+    }
+    return dayCounts[3] + dayCounts[6];
+}
+
+function getSportsLeft(dayCounts, currentDay) {
+    const weekDayTime = { startTime: '15:30', endTime: '17:45' };
+    const weekEndTime = { startTime: '13:00', endTime: '16:00' };
+
+    if (currentDay == 2 || currentDay == 4) { // Tue, Thur
+        return getRemainingInstances(weekDayTime) + dayCounts[1] + dayCounts[3] + dayCounts[5] - 1;
+    } else if (currentDay == 6) { // Saturday
+        return getRemainingInstances(weekEndTime) + dayCounts[1] + dayCounts[3] + dayCounts[5] - 1;
+    }
+    return dayCounts[1] + dayCounts[3] + dayCounts[5];
+}
+
+function get360Left(dayCounts, currentDay) {
+    const weekDayTime = { startTime: '15:30', endTime: '17:45' };
+    const weekEndTime = { startTime: '13:00', endTime: '16:00' };
+
+    // Mon, Wed
+    if (currentDay == 1 || currentDay == 3) {
+        return getRemainingInstances(weekDayTime) + dayCounts[1] + dayCounts[3] + dayCounts[5] - 1;
+    } 
+    return dayCounts[1] + dayCounts[3];
+}
+
+function getBlocksLeft(weekDayCount, isWeekday, blockTimes) {
+    const blocksLeftToday = getRemainingInstances(blockTimes);
+    if (isWeekday) {
+        return blocksLeftToday + ((weekDayCount-1) * 4);
+    } else {
+        return weekDayCount * 4;
+    }
+}
+
+function getIHLeft(interhouseDates, now) {
     let IHCount = 0;
     interhouseDates.forEach(date => {
         if (date.getTime() > now) {
             IHCount++;
         }
     });
-    document.getElementById("IHCount").innerHTML = IHCount;
-
-    const timeLeft = graduationDate - now;
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    document.getElementById("mealCount").innerHTML = ((days * 3) - dayCounts[6]); // subtract suns cuz there is only 2 meals
-
+    return IHCount;
 }
 
-window.onload = function() {
-    extraCount();
-};
+function getMealsLeft(weekDayCount, dayCounts, mealTimes, sunMealTimes, currentDay) {
+    let mealsLeft = 0;
+    const nonSundays = weekDayCount + dayCounts[6];
+    if (currentDay != 0) { // Not a sunday
+        const mealsLeftToday = getRemainingInstances(mealTimes);
+        mealsLeft = mealsLeftToday + ((nonSundays - 1) * 3) + (dayCounts[0] * 2); // 2 meals on sundays
+    }
+    else { // Is a Sunday
+        const mealsLeftToday = getRemainingInstances(sunMealTimes);
+        mealsLeft = mealsLeftToday + ((nonSundays) * 3) + ((dayCounts[0] - 1) * 2);
+    }
+    return mealsLeft;
+}
 
 const countDays = (startDate, endDate, dayOfWeek) => {
   let count = 0;
@@ -76,4 +151,34 @@ const countDays = (startDate, endDate, dayOfWeek) => {
   }
 
   return count;
+};
+
+function getCurrentTimeInMinutes() {
+    const now = new Date();
+    return now.getHours() * 60 + now.getMinutes();
+}
+
+// Gets how many more times something will occur in the day
+function getRemainingInstances(times) {
+    const currentDate = new Date();
+    
+    const currentTime = getCurrentTimeInMinutes();
+    let remainingInstances = 0;
+
+    times.forEach(block => {
+        const [startHour, startMinute] = block.startTime.split(':').map(Number);
+
+        // convert start time to mins since midnight
+        const startTimeInMinutes = (startHour * 60) + startMinute;
+
+        if (currentTime < startTimeInMinutes) {
+            remainingInstances++;
+        }
+    });
+
+    return remainingInstances;
+}
+
+window.onload = function() {
+    getInfo();
 };
